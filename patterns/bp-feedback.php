2139e4d438f757ed79446903c745be4d61ffd2d8
imath <math.viet@gmail.com>
2023-07-14 11:43:29 +0200

    Use `wp/title` block and introduce a BP pattern

---
+++ patterns/bp-feedback.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: BP Feedback
+ * Slug: buddyvibes/bp-feedback
+ * Inserter: no
+ *
+ * @package /buddyvibes/patterns
+ *
+ * @since 1.0.0
+ */
+?>
+<!-- wp:group {"layout":{"type":"flex"}} -->
+<div class="wp-block-group">
+	<!-- wp:paragraph -->
+	<p><?php esc_html_e( 'This is an example of template part used into the default BuddyPress template.', 'buddyvibes' ); ?></p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
